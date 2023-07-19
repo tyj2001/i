1cc53e18fee6b11cbf448ccdb46887352910e0ce
tyj2001 <139536726+tyj2001@users.noreply.github.com>
2023-07-20 06:07:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,38 +2,40 @@ import requests
 import json
 
 def imagine_picture(prompt):
-    url = "http://midjourney-api.ai-des.com/func2api/Imagine"
-    headers = {
-        "Content-Type": "application/json",
-        "UUID": "afd22831-6e9e-c4e4-2477-825a3328a178"
-    }
-    data = {
-        "type": "P",
-        "prompt": prompt,
-        "imgId": "",
-        "num": 0
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    if response.status_code == 200:
+    try:
+        url = "http://midjourney-api.ai-des.com/func2api/Imagine"
+        headers = {
+            "Content-Type": "application/json",
+            "UUID": "afd22831-6e9e-c4e4-2477-825a3328a178"
+        }
+        data = {
+            "type": "P",
+            "prompt": prompt,
+            "imgId": "",
+            "num": 0
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        response.raise_for_status()
         return response.json()["data"]["imgId"]
-    else:
+    except Exception as e:
+        print("Failed to generate image. Error:", e)
         return None
 
 def get_picture_url(img_id):
-    url = "http://midjourney-api.ai-des.com/func2api/GetPicByImgID"
-    headers = {
-        "Content-Type": "application/json",
-        "UUID": "afd22831-6e9e-c4e4-2477-825a3328a178"
-    }
-    data = {
-        "imgId": img_id
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    print(f"Status code: {response.status_code}")
-    print(f"Response content: {response.content}")  # This will print the entire response
-    if response.status_code == 200:
+    try:
+        url = "http://midjourney-api.ai-des.com/func2api/GetPicByImgID"
+        headers = {
+            "Content-Type": "application/json",
+            "UUID": "afd22831-6e9e-c4e4-2477-825a3328a178"
+        }
+        data = {
+            "imgId": img_id
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        response.raise_for_status()
         return response.json()["data"]["url"]
-    else:
+    except Exception as e:
+        print(f"Failed to get image URL. Error:", e)
         return None
 
 def main():
